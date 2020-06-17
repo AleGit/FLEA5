2aa8a32b6f78a45e9181929a40509ab2a91d4468
Alexander Maringele <alexander.maringele@gmail.com>
2020-06-17 12:17:05 +0200

    Updated main

--- Sources/Flea/main.swift
+++ Sources/Flea/main.swift
@@ -4,7 +4,7 @@ import Tptp
 print("FLEA - First order Logic with Equality Attester (FLEA)")
 print("======================================================")
 
-Syslog.openLog(options: .console, .pid, .perror, verbosely: false)
+Syslog.openLog(options: .console, .pid, .perror)
 defer { Syslog.closeLog() }
 
 Syslog.notice { "{ \(Syslog.defaultLogLevel), \(Syslog.logLevel()) } ⊆ [ \(Syslog.minimalLogLevel), \(Syslog.maximalLogLevel) ]" }
