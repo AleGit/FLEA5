9a4cf0cab10fcf76fd112cd61419cb8a0b8f4534
Alexander Maringele <alexander.maringele@gmail.com>
2020-06-18 11:48:01 +0200

    Updated Z3 version

--- Tests/SolverTests/Z3SATSolverTests.swift
+++ Tests/SolverTests/Z3SATSolverTests.swift
@@ -6,7 +6,7 @@ final class Z3SATSolverTests : Z3TestCase, HasSolver {
     lazy var solver: Solver = Z3SATSolver()
 
     func testVersion() {
-        let expected = "Z3 • 4.5.1.0"
+        let expected = "Z3 • 4.8.8.0"
         let actual = solver.description
 
         Syslog.notice { actual }
