6237bc6e8dad136efb74325382655187b08112e6
Alexander Maringele <alexander.maringele@gmail.com>
2020-06-20 19:14:00 +0200

    Testing Tptp.Sequence

--- Tests/TptpTests/TptpSequenceTests.swift
+++ Tests/TptpTests/TptpSequenceTests.swift
@@ -3,49 +3,14 @@ import XCTest
 import Runtime
 
 class TptpSequenceTests : AbstractTestCase {
-    func testFibonacci() {
-        let fibonacci = Tptp.Sequence(
-                first: (1, 1),
-                step: { (s, t) in
-                    guard s <= 100 else {
-                        return nil
-                    }
-                    return (t, s + t)
-                },
-                data: { (s, _) in s })
 
-        let list = fibonacci.map {
-            $0
-        }
-        XCTAssertEqual([1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144], list)
-    }
-
-    func testEvenFibonacci() {
-        let fibonacci = Tptp.Sequence(
-                first: (1, 1),
-                step: { (s, t) in
-                    guard s <= 200 else {
-                        return nil
-                    }
-                    return (t, s + t)
-                },
-                where: { (s, _) in
-                    return s % 2 == 0
-                },
-                data: { (s, _) in s })
-
-        let list = fibonacci.map {
-            $0
-        }
-        XCTAssertEqual([2, 8, 34, 144], list)
-    }
 }
 
 extension TptpSequenceTests {
 
     private static var primes10 = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29]
 
-    private class Simples: Sequence {
+    class Simples: Sequence {
         private var sorted = primes10
         private lazy var primes = Set(sorted)
 
@@ -87,23 +52,46 @@ extension TptpSequenceTests {
     class Primes: Sequence {
         private var sorted = primes10
 
-        private func prime(s: Int) -> Bool {
-
+        private func sieved(s: Int) -> Bool {
+            assert(sorted.last! < s)
             for p in sorted[1...] {
                 if s % p == 0 {
-                    return false
+                    return true // sieved, i.e. not a prime
                 }
             }
             sorted.append(s)
-            return true
+            return false // not sieved, i.e. a prime
         }
 
         subscript(idx: Int) -> Int {
             while idx >= sorted.count {
                 var s = sorted.last! + 2
-                while !prime(s: s) { s += 2}
+                while sieved(s: s) { s += 2}
             }
             return sorted[idx]
+        }
+
+        func prime(p: Int) -> Bool {
+            while p >= sorted.last! {
+                var s = sorted.last! + 2
+                while sieved(s: s) { s += 2 }
+            }
+
+            // binary search
+            var lhs = 0
+            var rhs = sorted.count - 1
+
+            while lhs <= rhs {
+                let mid = (lhs+rhs)/2
+                let val = sorted[mid]
+
+                if val == p { return true }
+
+                if val < p { lhs = mid+1 }
+                else { rhs = mid-1 }
+            }
+            return false
+
         }
 
         func makeIterator() -> Tptp.Iterator<Int, Int> {
@@ -119,6 +107,19 @@ extension TptpSequenceTests {
             }
             return Array(sorted[0..<k])
         }
+    }
+
+    func testPrime() {
+        let primes = Primes()
+
+        for n in 0...TptpSequenceTests.primes10.last! {
+            XCTAssertEqual(TptpSequenceTests.primes10.contains(n), primes.prime(p: n) )
+        }
+
+        for p in TptpSequenceTests.primes10.reversed() {
+            XCTAssertTrue(primes.prime(p: p))
+        }
+
     }
 
     func testPrimes() {
@@ -158,7 +159,45 @@ extension TptpSequenceTests {
             simples[2100]
         }
 
+        XCTAssertTrue(primes.prime(p: s))
+        XCTAssertTrue(primes.prime(p: p))
         XCTAssertEqual(s, p)
         XCTAssertTrue(time_p < time_s, "time_p=\(time_p.absolute) • time_s=\(time_s.absolute)")
+    }
+}
+
+extension TptpSequenceTests {
+
+    private static var fibonacci13 = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144]
+
+    class FibonacciSequence: Sequence {
+        private var sorted = [0, 1]
+
+        subscript(idx: Int) -> Int {
+            while idx >= sorted.count {
+                let n = sorted[sorted.count-2] + sorted[sorted.count-1]
+                sorted.append(n)
+            }
+            return sorted[idx]
+        }
+
+        func makeIterator() -> Tptp.Iterator<Int, Int> {
+            return Tptp.Iterator(
+                    first: 0,
+                    step: { idx in idx + 1  },
+                    data: { idx in self[idx] })
+        }
+
+        func first(_ k: Int) -> [Int] {
+            for _ in self {
+                guard sorted.count < k else { break }
+            }
+            return Array(sorted[0..<k])
+        }
+    }
+
+    func testFibonacci() {
+        let fibs = FibonacciSequence()
+        XCTAssertEqual(TptpSequenceTests.fibonacci13, fibs.first(13))
     }
 }
