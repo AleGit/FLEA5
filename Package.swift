94ee7bd6e77a4ec588c55f1fdcaae0a476292be1
Alexander Maringele <alexander.maringele@gmail.com>
2020-06-21 17:42:18 +0200

    Simplified test suite definition

--- Package.swift
+++ Package.swift
@@ -3,33 +3,6 @@
 
 import PackageDescription
 
-extension Target {
-    enum Module: String {
-        case base = "Base"
-        case utile = "Utile"
-        case tptp = "Tptp"
-        case solver = "Solver"
-        case flea = "Flea"
-
-        var targetName: String {
-            self.rawValue
-        }
-        var testTargetName: String {
-            self.targetName + "Tests"
-        }
-        var targetDependency : Target.Dependency {
-            Target.Dependency.init(stringLiteral: self.rawValue)
-        }
-    }
-    static func target(module: Module, dependencies: [Module]) -> PackageDescription.Target {
-        return .target(name: module.targetName, dependencies: dependencies.map { $0.targetDependency })
-    }
-    static func testTarget(module: Module, dependencies: [Module]) -> PackageDescription.Target {
-        return .testTarget(name: module.testTargetName, dependencies: dependencies.map { $0.targetDependency })
-    }
-}
-
-
 let package: Package = Package(
     name: "FLEA5",
     dependencies: [
@@ -47,11 +20,50 @@ let package: Package = Package(
         .target(module: .utile,  dependencies: [.base]),
         .target(module: .tptp,   dependencies: [.base, .utile]),
         .target(module: .solver, dependencies: [.base, .utile, .tptp]),
-        .target(module: .flea,   dependencies: [.base, .utile, .tptp]),
+        .target(module: .flea,   dependencies: [.base, .utile, .tptp, .solver]),
 
-        .testTarget(module: .base,   dependencies: [.base]),
-        .testTarget(module: .utile,  dependencies: [.utile]),
-        .testTarget(module: .tptp,   dependencies: [.tptp]),
-        .testTarget(module: .solver, dependencies: [.solver]),
+        .testTarget(module: .base),
+        .testTarget(module: .utile),
+        .testTarget(module: .tptp),
+        .testTarget(module: .solver),
     ]
 )
+
+
+
+extension Target {
+    /// Since literal strings are used multiple times for names and dependencies
+    /// we introduce module enum values for all our modules and test suits.
+    enum Module: String {
+        case base = "Base"
+        case utile = "Utile"
+        case tptp = "Tptp"
+        case solver = "Solver"
+        case flea = "Flea"
+
+        var targetName: String {
+            self.rawValue
+        }
+        var testTargetName: String {
+            self.targetName + "Tests"
+        }
+        var targetDependency : Target.Dependency {
+            Target.Dependency.init(stringLiteral: self.rawValue)
+        }
+    }
+    /// We introduce a factory method that uses module enum values for name and dependencies of a module.
+    static func target(module: Module, dependencies: [Module]) -> PackageDescription.Target {
+        return .target(name: module.targetName, dependencies: dependencies.map { $0.targetDependency })
+    }
+    /// We introduce a factory method that uses module enum values for name and dependencies of a test suite.
+    /// By convention we create for module "Foo" a test suite "FooTests" depending on module "Foo".
+    /// We must not create a test suite depending on an other test suite.
+    static func testTarget(module: Module, dependencies: [Module]? = nil) -> PackageDescription.Target {
+        guard let dependencies = dependencies else {
+            // implicit test suit name, implicit dependency on one module
+            return .testTarget(name: module.testTargetName, dependencies: [module.targetDependency])
+        }
+        // implicit test suite name, explicit dependency none, one or more modules
+        return .testTarget(name: module.testTargetName, dependencies: dependencies.map { $0.targetDependency} )
+    }
+}
