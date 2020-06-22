316638bfbb97b2eed6a5e3368852d02e538c9abc
Alexander Maringele <alexander.maringele@gmail.com>
2020-06-22 07:13:34 +0200

    Refactored Package.swift

--- Package.swift
+++ Package.swift
@@ -32,42 +32,46 @@ let package: Package = Package(
 )
 
 extension Target {
-    /// Since literal strings are used multiple times for names and dependencies
-    /// we introduce module enum values for all our modules and test suits.
+    /// Since string literals are used multiple times for names and dependencies
+    /// we introduce module enum values for all our modules and test suits
+    /// to avoid typos and enables easier renaming.
     enum Module: String {
-        case base   = "Base"   // "extension" of Foundation
-        case utile  = "Utile"  // protocols, data structures, and algorithms
+        case base   = "Base"   // extensions of Foundation and swift standard library
+        case utile  = "Utile"  // protocols and structs ("abstract data types")
         case tptp   = "Tptp"   // parsing and data structures for tptp files
         case solver = "Solver" // proving with yices and z3
         case flea   = "Flea"   // command line program definition
 
         /// the name of this module
-        var targetName: String {
-            self.rawValue
-        }
+        var targetName: String { self.rawValue }
         /// the name of the test suite for this module
-        var testTargetName: String {
-            self.targetName + "Tests"
-        }
-
-        /// the dependency on this module (by an other module or test suite)
-        var dependency: Dependency {
-            Dependency(stringLiteral: self.rawValue)
-        }
+        var testTargetName: String { self.targetName + "Tests" }
+        /// a dependency on this module (by an other module or test suite)
+        var dependency: Dependency { Dependency(stringLiteral: self.rawValue) }
     }
-    /// We introduce a factory method that uses module enum values for name and dependencies of a module.
+
+    /// Factory method that uses module enum values for name and dependencies of a module.
+    /// - Parameters:
+    ///   - module: the module enum value
+    ///   - dependencies: the packages the module is dependent on
+    /// - Returns: a regular package description build target
     static func target(module: Module, dependencies: [Module]) -> PackageDescription.Target {
         return .target(name: module.targetName, dependencies: dependencies.map { $0.dependency })
     }
-    /// We introduce a factory method that uses module enum values for name and dependencies of a test suite.
-    /// By convention we create for a module "Foo" a test suite "FooTests" with dependency on module "Foo".
-    /// We must not create a test suite depending on other test suites.
+
+    /// Factory method that uses module enum values for name and dependencies of a test suite.
+    /// By default a test suite "FooTests" is created with dependency on module "Foo".
+    /// A test suite must not depend on other test suites.
+    /// - Parameters:
+    ///   - module: the module enum value the test suite is for
+    ///   - dependencies: the packages the test suite is dependent on
+    /// - Returns: a test package description target
     static func testTarget(module: Module, dependencies: [Module]? = nil) -> PackageDescription.Target {
         guard let dependencies = dependencies else {
-            // implicit test suit name, implicit dependency on one module
+            // implicit test suit name, implicit dependency on one module, e.g. FooTests -> [Foo]
             return .testTarget(name: module.testTargetName, dependencies: [module.dependency])
         }
-        // implicit test suite name, explicit dependency none, one or more modules
+        // implicit test suite name, explicit dependency none, one or more modules, e.g. FooTests -> [Foo, Bar]
         return .testTarget(name: module.testTargetName, dependencies: dependencies.map { $0.dependency} )
     }
 }
