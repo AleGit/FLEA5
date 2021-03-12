2899a25a94fc0ec089eb20f75254f66ccaa668aa
Alexander Maringele <alexander@maringele.at>
2021-03-12 19:45:43 +0100

    Support different Z3 versions

--- Tests/SolverTests/Z3ContextPublicTests.swift
+++ Tests/SolverTests/Z3ContextPublicTests.swift
@@ -5,7 +5,11 @@ import XCTest
 class Z3ContextPublicTests: Z3TestCase {
 
   func testVersion() {
+    #if os(macOS)
     let expected = "Z3 • 4.8.10.0"
+    #else
+    let expected = "Z3 • 4.8.9.0"
+    #endif
     let actual = Z3.Context().description
 
     Syslog.notice { actual }
